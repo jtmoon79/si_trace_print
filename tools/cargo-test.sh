c6552f3c55437fddf3ee914a1036db9c0298261a
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2022-09-20 16:56:54 -0700

    cargo-test.sh add RUST_BACKTRACE if not present

--- tools/cargo-test.sh
+++ tools/cargo-test.sh
@@ -7,6 +7,10 @@ set -eu
 
 cd "$(dirname -- "${0}")/.."
 
+if [[ ! "${RUST_BACKTRACE+x}" ]]; then
+    export RUST_BACKTRACE=1
+fi
+
 set -x
 
 exec cargo test --verbose --future-incompat-report --all-features "${@}"
