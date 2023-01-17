a98fdf7afbc9ffed1db25f552aaa6e4132c9b83e
jtmoon79 <815261+jtmoon79@users.noreply.github.com>
2023-01-16 22:12:49 -0800

    add rust-workflow.sh

---
+++ tools/rust-workflow.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# Locally run the significant commands done in the
+# `.github/workflows/rust.yml` file.
+#
+# Developers must manually update this script when `rust.yml` changes.
+#
+
+set -euo pipefail
+
+cd "$(dirname -- "${0}")/.."
+
+set -x
+
+cargo msrv verify
+cargo build --lib --verbose
+cargo build --lib --verbose --release
+cargo test --lib --verbose --all-features --future-incompat-report
+cargo check --all-targets --release
+cargo clippy --no-deps --all-targets --all-features
+cargo doc --locked --release --frozen --no-deps
+cargo publish --dry-run --allow-dirty
+cargo fmt --verbose --check
+cargo clippy --no-deps --verbose --all-targets --all-features -- -D warnings
